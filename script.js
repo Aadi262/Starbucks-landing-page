c3522bad53eba1e5cf0e9f5aa721e2c73c457f1c
Aadi262 <84340941+Aadi262@users.noreply.github.com>
2023-07-13 01:59:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// let saveEl = document.getElementById("save-el");
+// let countEl = document.getElementById("count-el");
+// let count = 0;
+
+// function increment() {
+//   count += 1;
+//   countEl.innerText = count;
+// }
+
+// function save() {
+//   let countStr = count + " - ";
+//   saveEl.innerText += countStr;
+//   countEl.innerText =
+
+// }
+// let firstCard = 6;
+// let secondCard = 9;
+// let sum = firstCard + secondCard;
+// if (sum < 21) {
+//   console.log("Do You Want To Draw A New Card ");
+// } else if (sum === 21) {
+//   console.log("You Have Got the Black Jack");
+// } else if (sum > 21) {
+//   console.log("SOrry You Are Out Of The Game");
+// }
+
+// let firstCard = 1;
+// let secondCard = 10;
+// let sum = firstCard + secondCard;
+// let hash = false;
+// let isAlive = true;
+// let message = " ";
+// let messageEl = document.getElementById("message-el");
+// function startGame() {
+//   if (sum > 21) {
+//     message = "you are out  the game";
+//   } else if (sum === 21) {
+//     message = "you have won the game";
+//     hash = true;
+//   } else {
+//     message = "choose one more card";
+//     isAlive = false;
+//   }
+//   messageEl.textContent = message;
+//   console.log(message);
+// }
